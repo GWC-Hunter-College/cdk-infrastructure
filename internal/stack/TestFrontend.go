17852398b383fbf7c86b10fd0d31071a65f2b77e
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-06-25 00:56:44 -0400

    Attempt to move Frontend
    This is a the code that i think should be the equvilant go code for the frontend cdk
    i just need to test that this works, then we can replace the old typescript cdk fully

--- internal/stack/TestFrontend.go
+++ internal/stack/TestFrontend.go
@@ -1,11 +1,14 @@
 package stack
 
 import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"       // core
-	"github.com/aws/aws-cdk-go/awscdk/v2/awss3" // ← FIXED: v2 path!
+	"github.com/aws/aws-cdk-go/awscdk/v2" // core
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfront"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfrontorigins"
 )
 
 type FrontendStackProps struct {
@@ -24,7 +27,7 @@ func NewFrontendStack(scope constructs.Construct, id string, props *FrontendStac
 	websiteBucket := awss3.NewBucket(stack,
 		jsii.String("testBucket"), // logical ID
 		&awss3.BucketProps{
-			BucketName:        jsii.String("test-site-for-hunter"),
+			BucketName:        jsii.String("test-site-for-hunter-kyle"),
 			PublicReadAccess:  jsii.Bool(false),
 			RemovalPolicy:     awscdk.RemovalPolicy_DESTROY,
 			AutoDeleteObjects: jsii.Bool(true),
@@ -35,7 +38,60 @@ func NewFrontendStack(scope constructs.Construct, id string, props *FrontendStac
 		Value: websiteBucket.BucketName(),
 	})
 
-	// var cloudfrontDistribution cloudfront.Distribution
+	// authorization for the s3 bucket through oai
+	cloudfrontOAI := awscloudfront.NewOriginAccessIdentity(stack, jsii.String("CloudFrontOAI"), &awscloudfront.OriginAccessIdentityProps{})
+
+	websiteBucket.GrantRead(cloudfrontOAI, nil)
+
+	// first the main (can also be called staging)
+	// cloudfront configeration
+	cloudfrontMainBehavior := &awscloudfront.BehaviorOptions{
+		// Sets the S3 Bucket as the origin and tells CloudFront to use the created OAI to access it
+		Origin: awscloudfrontorigins.NewS3Origin(websiteBucket, &awscloudfrontorigins.S3OriginProps{
+			OriginAccessIdentity: cloudfrontOAI,
+			OriginPath:           jsii.String("/main"),
+			// OriginId:             jsii.String("CloudFrontS3Access"),
+		}),
+		ViewerProtocolPolicy: awscloudfront.ViewerProtocolPolicy_REDIRECT_TO_HTTPS,
+	}
+
+	var frontendMain awscloudfront.Distribution
+
+	frontendMain = awscloudfront.NewDistribution(stack, jsii.String("FrontendMain"), &awscloudfront.DistributionProps{
+		DefaultRootObject: jsii.String("index.html"),
+		DefaultBehavior:   cloudfrontMainBehavior,
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("CloudFront_Main_Info"), &awscdk.CfnOutputProps{
+		Description: jsii.String("Main Branch CloudFront Info"),
+		Value: jsii.String("Main URL: https://" + *frontendMain.DomainName() +
+			" | ID: " + *frontendMain.DistributionId()),
+	})
+
+	// first the main (can also be called staging)
+	// cloudfront configeration
+	cloudfrontProductionBehavior := &awscloudfront.BehaviorOptions{
+		// Sets the S3 Bucket as the origin and tells CloudFront to use the created OAI to access it
+		Origin: awscloudfrontorigins.NewS3Origin(websiteBucket, &awscloudfrontorigins.S3OriginProps{
+			OriginAccessIdentity: cloudfrontOAI,
+			OriginPath:           jsii.String("/production"),
+			// OriginId:             jsii.String("CloudFrontS3Access"),
+		}),
+		ViewerProtocolPolicy: awscloudfront.ViewerProtocolPolicy_REDIRECT_TO_HTTPS,
+	}
+
+	var frontendProduction awscloudfront.Distribution
+
+	frontendProduction = awscloudfront.NewDistribution(stack, jsii.String("FrontendProduction"), &awscloudfront.DistributionProps{
+		DefaultRootObject: jsii.String("index.html"),
+		DefaultBehavior:   cloudfrontProductionBehavior,
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("CloudFront_Production_Info"), &awscdk.CfnOutputProps{
+		Description: jsii.String("Production Branch CloudFront Info"),
+		Value: jsii.String("Production URL: https://" + *frontendProduction.DomainName() +
+			" | ID: " + *frontendProduction.DistributionId()),
+	})
 
 	return stack
 }
