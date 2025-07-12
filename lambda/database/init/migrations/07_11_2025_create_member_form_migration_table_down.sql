5667582f4395290a3fa61b2754ceecd585370541
lausan3 <lauanthony4455@gmail.com>
2025-07-12 18:55:30 -0400

    refactor: fix misnamed migration names

--- lambda/database/init/migrations/07_11_2025_create_member_form_migration_table_down.sql
+++ lambda/database/init/migrations/07_11_2025_create_member_form_migration_table_down.sql
@@ -1,12 +1,3 @@
-CREATE TABLE `MEMBER_FORM_DATA` (
-  `email` VARCHAR(255),
-  `full_name` VARCHAR(255),
-  `major` VARCHAR(255),
-  `emplid` VARCHAR(255),
-  `grad_year` int,
-  `dietary_restrictions` VARCHAR(255),
-  `comments` VARCHAR(255),
-  `join_date` timestamp
-);
+DROP TABLE IF EXISTS `MEMBER_FORM_DATA`;
 
 ALTER TABLE `MEMBER_FORM_DATA` COMMENT = 'Staging table to insert into students and student_info tables';
--- lambda/database/init/migrations/07_11_2025_create_member_form_migration_table_up copy.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS `MEMBER_FORM_DATA`;
-
-ALTER TABLE `MEMBER_FORM_DATA` COMMENT = 'Staging table to insert into students and student_info tables';
