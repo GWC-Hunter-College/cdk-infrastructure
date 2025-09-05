fc353a924764ae0860ac7ad16d8e283cd8f2a034
lausan3 <lauanthony4455@gmail.com>
2025-09-05 18:32:46 -0400

    refactor: refactor backend to host all images in events system in one bucket

--- internal/stack/images.go
+++ internal/stack/images.go
@@ -3,7 +3,6 @@ package stack
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
-	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -12,57 +11,27 @@ type ImageStackProps struct {
 	Props awscdk.StackProps
 }
 
-func NewImageStack(scope constructs.Construct, id string, props *ImageStackProps) awscdk.Stack {
+func NewImageStack(scope constructs.Construct, id string, props *ImageStackProps) (awscdk.Stack, awss3.IBucket) {
 	var sprops awscdk.StackProps
 	if props != nil {
 		sprops = props.Props
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	_ = awss3.NewBucket(stack, jsii.String("club-thumbnails-bucket"), &awss3.BucketProps{
-		BucketName: jsii.String("hc-club-thumbnails"),
+	imagesBucket := awss3.NewBucket(stack, jsii.String("hc-images-bucket"), &awss3.BucketProps{
+		BucketName: jsii.String("hunter-event-sys-uploaded-images"),
 		Cors: &[]*awss3.CorsRule{
 			{
 				AllowedMethods: &[]awss3.HttpMethods{
 					awss3.HttpMethods_GET,
 					awss3.HttpMethods_PUT,
-				},
-				AllowedOrigins: &[]*string{
-					jsii.String("*"), // allowing from all origins atm, should be locked down later
-				},
-			},
-		},
-		BlockPublicAccess: awss3.BlockPublicAccess_BLOCK_ALL(),
-		EnforceSSL:        jsii.Bool(true),
-	})
-
-	_ = awss3.NewBucket(stack, jsii.String("event-thumbnails-bucket"), &awss3.BucketProps{
-		BucketName: jsii.String("hc-event-thumbnails"),
-		Cors: &[]*awss3.CorsRule{
-			{
-				AllowedMethods: &[]awss3.HttpMethods{
-					awss3.HttpMethods_GET,
-					awss3.HttpMethods_PUT,
-				},
-				AllowedOrigins: &[]*string{
-					jsii.String("*"), // allowing from all origins atm, should be locked down later
-				},
-			},
-		},
-		BlockPublicAccess: awss3.BlockPublicAccess_BLOCK_ALL(),
-		EnforceSSL:        jsii.Bool(true),
-	})
-
-	bucket := awss3.NewBucket(stack, jsii.String("event-images-bucket"), &awss3.BucketProps{
-		BucketName: jsii.String("hc-event-images"),
-		Cors: &[]*awss3.CorsRule{
-			{
-				AllowedMethods: &[]awss3.HttpMethods{
-					awss3.HttpMethods_GET,
 					awss3.HttpMethods_POST,
 				},
 				AllowedOrigins: &[]*string{
 					jsii.String("*"), // allowing from all origins atm, should be locked down later
+				},
+				AllowedHeaders: &[]*string{
+					jsii.String("*"),
 				},
 			},
 		},
@@ -72,15 +41,13 @@ func NewImageStack(scope constructs.Construct, id string, props *ImageStackProps
 
 	// TODO: Add Event Notification to trigger a Lambda to add image details to RDS
 
-	pushDetailsArn := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("Insert Images into RDS Function"), &awscdklambdagoalpha.GoFunctionProps{
-		FunctionName: jsii.String("InsertS3ImageDetails"),
-		Entry:        jsii.String("./lambda/images/main.go"),
-	})
+	// Needs to be generic for each endpoint that adds images
+	// insertImageInDbLambda := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("Insert Images into RDS Function"), &awscdklambdagoalpha.GoFunctionProps{
+	// 	FunctionName: jsii.String("InsertS3ImageDetails"),
+	// 	Entry:        jsii.String("./lambda/images/main.go"),
+	// })
 
-	bucket.AddEventNotification(awss3.EventType_OBJECT_CREATED, awss3.BucketNotificationDestinationConfig{
-		Arn:  nil,
-		Type: awss3.BucketNotificationDestinationType_LAMBDA,
-	}, nil)
+	// imagesBucket.AddEventNotification(awss3.EventType_OBJECT_CREATED, awss3notifications.NewLambdaDestination(insertImageInDbLambda), nil)
 
-	return stack
+	return stack, imagesBucket
 }
