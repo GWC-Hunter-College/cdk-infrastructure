dc18e86a1a7e1302eb773016fc9dabd840697b50
lausan3 <lauanthony4455@gmail.com>
2025-11-05 16:07:02 -0500

    feat: add club description to get club by id endpoint

--- lambda/api/clubs/clubId/get.go
+++ lambda/api/clubs/clubId/get.go
@@ -58,12 +58,6 @@ func handler(ctx context.Context, request events.APIGatewayV2HTTPRequest) (event
 			nil,
 		)
 	}
-	desc := "A community of students empowering each other through coding workshops, mentorship, and projects. " +
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-		"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-		"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-
-	club.Description = &desc
 
 	response := map[string]any{
 		"club": club,
