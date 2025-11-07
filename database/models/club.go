609e5dc0534bf2bf3dc7748d57e3dc02659e090c
lausan3 <lauanthony4455@gmail.com>
2025-11-06 21:09:53 -0500

    fix: remove required validate tag on club thumbnail url

--- database/models/club.go
+++ database/models/club.go
@@ -3,7 +3,7 @@ package models
 type Club struct {
 	ID           int     `json:"id,omitempty" db:"id"`
 	Name         string  `json:"name" db:"name" validate:"required"`
-	ThumbnailURL *string `json:"thumbnailUrl,omitempty" db:"thumbnail_url" validate:"required"`
+	ThumbnailURL *string `json:"thumbnailUrl,omitempty" db:"thumbnail_url"`
 }
 
 type ClubRole string
