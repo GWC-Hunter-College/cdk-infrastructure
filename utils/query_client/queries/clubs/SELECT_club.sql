d444e2d67fcc44fb2832ac5192af65fe757a49d5
lausan3 <lauanthony4455@gmail.com>
2025-12-07 14:45:56 -0500

    refactor: revert GET /clubs/{clubId} endpoint to show unverified clubs

--- utils/query_client/queries/clubs/SELECT_club.sql
+++ utils/query_client/queries/clubs/SELECT_club.sql
@@ -6,7 +6,6 @@ SELECT
   ci.description AS description,
   ci.website_url AS website_url
 FROM clubs c
-INNER JOIN verified_clubs vc ON vc.fk_club_id = c.id
 LEFT JOIN images     i  ON i.id = c.fk_logo_id
 LEFT JOIN club_info  ci ON ci.fk_club_id = c.id
 WHERE c.id = ?
