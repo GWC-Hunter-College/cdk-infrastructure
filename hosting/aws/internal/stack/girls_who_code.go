83100d35b3a442c00a371f11b906be8657bbb842
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2026-09-02 15:57:21 -0400

    feat(hosting): add GWC CI deployment infrastructure
    Give the Girls Who Code hosting stack the same deployment capabilities as Hunter College Clubs: scoped S3 object and bucket access, invalidation rights for its staging and production distributions, a dedicated CI user and policy, and S3 destination outputs. Preserve all existing hosting resources and continue using OAI.

--- hosting/aws/internal/stack/girls_who_code.go
+++ hosting/aws/internal/stack/girls_who_code.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfront"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfrontorigins"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -99,6 +100,61 @@ func NewGirlsWhoCodeHostingStack(scope constructs.Construct, id string, props *G
 		Description: jsii.String("Production Branch CloudFront Info"),
 		Value: jsii.String("Production URL: https://" + *girlsWhoCodeProductionDistribution.DomainName() +
 			" | ID: " + *girlsWhoCodeProductionDistribution.DistributionId()),
+	})
+
+	girlsWhoCodeAccount := awscdk.Stack_Of(girlsWhoCodeHostingStack).Account()
+	girlsWhoCodeStagingDistributionARN := awscdk.Arn_Format(&awscdk.ArnComponents{
+		Service:      jsii.String("cloudfront"),
+		Account:      girlsWhoCodeAccount,
+		Resource:     jsii.String("distribution"),
+		ResourceName: girlsWhoCodeStagingDistribution.DistributionId(),
+		Region:       jsii.String(""),
+	}, girlsWhoCodeHostingStack)
+	girlsWhoCodeProductionDistributionARN := awscdk.Arn_Format(&awscdk.ArnComponents{
+		Service:      jsii.String("cloudfront"),
+		Account:      girlsWhoCodeAccount,
+		Resource:     jsii.String("distribution"),
+		ResourceName: girlsWhoCodeProductionDistribution.DistributionId(),
+		Region:       jsii.String(""),
+	}, girlsWhoCodeHostingStack)
+
+	girlsWhoCodeS3ObjectsStatement := awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Effect:    awsiam.Effect_ALLOW,
+		Actions:   jsii.Strings("s3:PutObject", "s3:DeleteObject"),
+		Resources: jsii.Strings(*girlsWhoCodeWebsiteBucket.ArnForObjects(jsii.String("*"))),
+	})
+	girlsWhoCodeS3ListStatement := awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Effect:    awsiam.Effect_ALLOW,
+		Actions:   jsii.Strings("s3:ListBucket"),
+		Resources: jsii.Strings(*girlsWhoCodeWebsiteBucket.BucketArn()),
+	})
+	girlsWhoCodeCloudFrontInvalidateStatement := awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Effect:    awsiam.Effect_ALLOW,
+		Actions:   jsii.Strings("cloudfront:CreateInvalidation"),
+		Resources: jsii.Strings(*girlsWhoCodeStagingDistributionARN, *girlsWhoCodeProductionDistributionARN),
+	})
+
+	girlsWhoCodeCIPolicy := awsiam.NewPolicy(girlsWhoCodeHostingStack, jsii.String("GirlsWhoCodeCiPolicy"), &awsiam.PolicyProps{
+		PolicyName: jsii.String("frontend-gwc-ci-policy"),
+		Statements: &[]awsiam.PolicyStatement{
+			girlsWhoCodeS3ObjectsStatement,
+			girlsWhoCodeS3ListStatement,
+			girlsWhoCodeCloudFrontInvalidateStatement,
+		},
+	})
+
+	girlsWhoCodeCIUser := awsiam.NewUser(girlsWhoCodeHostingStack, jsii.String("GirlsWhoCodeCiUser"), &awsiam.UserProps{
+		UserName: jsii.String("gwc-website-ci-deployer"),
+	})
+	girlsWhoCodeCIPolicy.AttachToUser(girlsWhoCodeCIUser)
+
+	awscdk.NewCfnOutput(girlsWhoCodeHostingStack, jsii.String("GirlsWhoCodeS3StagingDestination"), &awscdk.CfnOutputProps{
+		Description: jsii.String("Girls Who Code staging S3 deployment destination"),
+		Value:       jsii.String("s3://" + *girlsWhoCodeWebsiteBucket.BucketName() + "/staging"),
+	})
+	awscdk.NewCfnOutput(girlsWhoCodeHostingStack, jsii.String("GirlsWhoCodeS3ProductionDestination"), &awscdk.CfnOutputProps{
+		Description: jsii.String("Girls Who Code production S3 deployment destination"),
+		Value:       jsii.String("s3://" + *girlsWhoCodeWebsiteBucket.BucketName() + "/production"),
 	})
 
 	return girlsWhoCodeHostingStack
